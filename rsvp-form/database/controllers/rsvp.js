2aeba82b7b02e5af71f4b218ac252615e94a99bd
one-summers-day <jiro.horikoshi1@gmail.com>
2022-01-22 19:36:23 -0600

    Should work . . . all the pieces are there. Too tired to debug and test. .

--- rsvp-form/database/controllers/rsvp.js
+++ rsvp-form/database/controllers/rsvp.js
@@ -1,6 +1,8 @@
 const Rsvp = require('../models/rsvp.js');
 
-const findRsvpAndUpdate = () => {
-  
+const findRsvpAndUpdate = (f, l, e, g) => {
+  Rsvp.collection.insertOne(
+    {firstName: f, lastName: l, email: e, guests: g}
+  )
 };
 module.exports = findRsvpAndUpdate;
